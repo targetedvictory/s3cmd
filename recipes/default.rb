9b6b13d4c6a0d13dd4db495422ad5f760fde08d0
ssemichev <ssemichev@targetedvictory.com>
2014-11-17 17:45:53 -0500

    Update app_name_extension attribute

--- recipes/default.rb
+++ recipes/default.rb
@@ -35,7 +35,7 @@ end
 node[:s3cmd][:users].each do |user|   
   home = user.to_s == :root.to_s ? "/root" : "/home/#{user}"
   
-  template "s3cfg#{app_name}" do
+  template "s3cfg#{node[:s3cmd][:app_name_extension]}" do
       path "#{home}/.s3cfg#{node[:s3cmd][:app_name_extension]}"
       source "s3cfg.erb"
       owner "#{user}"
